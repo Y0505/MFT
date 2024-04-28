033a1fd7f8b0ad016c042a47c07fc173c6075bb7
Yaser Montazeri <76882166+Y0505@users.noreply.github.com>
2024-04-28 15:30:00 +0330

    Add files via upload

---
+++ MFT/31/js.js
@@ -0,0 +1,6 @@
+let userInput = document.querySelector('#userInput');
+let sourceUnit = document.querySelector("select[name='sourceUnit']");
+let desinationUnit = document.querySelector("select[name='desinationUnit']");
+
+
+console.log();
