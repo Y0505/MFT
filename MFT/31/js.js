a8a6ab6304af001dcf16da14bae74d6069b29cb0
Yaser Montazeri <76882166+Y0505@users.noreply.github.com>
2024-04-30 18:14:37 +0330

    Delete MFT/31 directory

--- MFT/31/js.js
+++
@@ -1,6 +0,0 @@
-let userInput = document.querySelector('#userInput');
-let sourceUnit = document.querySelector("select[name='sourceUnit']");
-let desinationUnit = document.querySelector("select[name='desinationUnit']");
-
-
-console.log();
