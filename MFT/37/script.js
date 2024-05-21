60276b96ef0b11fc1db477bc14f1f116eff3f7b7
Yaser Montazeri <76882166+Y0505@users.noreply.github.com>
2024-05-21 18:17:03 +0330

    Add files via upload

---
+++ MFT/37/script.js
@@ -0,0 +1,15 @@
+//#region numbers
+    const con = document.createElement("div")
+    con.style.display = 'flex';
+    con.style.width = '800px';
+    con.style.height = '100px';
+    con.style.backgroundColor = 'gray';
+    document.body.appendChild(con);
+
+    const con = document.createElement("div")
+    con.style.display = 'flex';
+    con.style.width = '800px';
+    con.style.height = '100px';
+    con.style.backgroundColor = 'gray';
+    document.body.appendChild(con);
+//#endregion
